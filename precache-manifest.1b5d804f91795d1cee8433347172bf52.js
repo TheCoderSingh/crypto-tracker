8be70f39b8c833cf36bae59a659680755bf72235
Jaskaran Singh <jaskaran1523@gmail.com>
2020-09-01 23:48:21 -0700

    Updates

---
+++ precache-manifest.1b5d804f91795d1cee8433347172bf52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c593517724a80ea28d1ec695513bac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb7b2fd0e469ae87c525",
+    "url": "/static/css/main.5d6ab243.chunk.css"
+  },
+  {
+    "revision": "e042156ecda685a4d548",
+    "url": "/static/js/2.86bab506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86bab506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7b2fd0e469ae87c525",
+    "url": "/static/js/main.be21dcca.chunk.js"
+  },
+  {
+    "revision": "392e57d1358b9e5f592d",
+    "url": "/static/js/runtime-main.69a76cdf.js"
+  }
+]);
